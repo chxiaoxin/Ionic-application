ba3f88118e29be43cd523f52b5e42a5c37b71d17
chxiaoxin <e0001836@u.nus.edu>
2016-09-01 22:49:36 +0800

    add ionic card class

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('firstApp', ['ionic', 'firstApp.controllers'])
+angular.module('firstApp', ['ionic', 'firstApp.controllers','firstApp.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -36,7 +36,8 @@ angular.module('firstApp', ['ionic', 'firstApp.controllers'])
     url: '/home',
     views: {
       'menuContent': {
-        templateUrl: 'templates/home.html'
+        templateUrl: 'templates/home.html',
+          controller:'IndexController'
       }
     }
   })
@@ -71,7 +72,7 @@ angular.module('firstApp', ['ionic', 'firstApp.controllers'])
     url: '/menu/:id',
     views: {
       'menuContent': {
-        templateUrl: 'templates/dishdetail.html',
+        templateUrl: 'templates/dishdetails.html',
         controller: ''
       }
     }
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('firstApp.controllers', [])
+angular.module('firstApp.controllers', ['ngResource'])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
@@ -40,17 +40,141 @@ angular.module('firstApp.controllers', [])
     }, 1000);
   };
 })
+.controller("MenuController",['$scope','menuFactory',function($scope,menuFactory){
+           $scope.filterText="";
+           $scope.showDetails=false;
+           $scope.setTab=1;
+           $scope.showMenu=false;
+           $scope.message='loading...';
+           $scope.dishes=menuFactory.getdishes().query(
+               function(response){
+                   $scope.dishes=response;
+                   $scope.showMenu=true;
+               },function(response){
+                   $scope.message='Error'+response.status+response.statusText;
+               });
+            $scope.select=function(setTab){
+                $scope.setTab=setTab;
+                switch(setTab){
+                    case 1:
+                        $scope.filterText='';
+                        break;
+                    case 2:
+                        $scope.filterText='mains';
+                        break;
+                    case 3:
+                        $scope.filterText='appetizer';
+                        break;
+                    case 4:
+                        $scope.filterText='dessert';
+                        break;
+                    default:
+                        break;
+                }    
+            };
+            $scope.toggleDetails=function(){
+                $scope.showDetails=!$scope.showDetails;
+            };
+            $scope.isSelected=function(setTab){
+                return $scope.setTab===setTab;
+            };
+        }])
+.controller('ContactController',['$scope',function($scope){
+    $scope.feedback={firstname:'',
+                     lastname:'',
+                     agree:false,
+                     mychannel:'',
+                     email:''
+                    };
+    $scope.channels=[{value:'tel',label:'tel'},{value:'Email',label:'Email'}];
+    $scope.invalidchannelselection=false;
+}])
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+.controller('FeedbackController',['$scope',function($scope){
+    $scope.submit=function(){
+         console.log($scope.feedback);
+                                if ($scope.feedback.agree && ($scope.feedback.mychannel === "")&& !$scope.feedback.mychannel) {
+                                    $scope.invalidchannelselection = true;
+                                    console.log('incorrect');
+                                  }else{
+                                  $scope.invalidchannelselection = false;
+                                  $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                                  agree:false, email:"" };
+                                  $scope.feedback.mychannel="";
+                                  $scope.feedbackform.$setPristine();
+                                  console.log($scope.feedback);
+                                  }
+                                  };
+}])
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+.controller('dishDetailController',['$scope','menuFactory','$stateParams',function($scope,menuFactory,$stateParams){
+            $scope.test={};
+            $scope.criteria;
+            $scope.showDish=false;
+            $scope.message='Loading...';
+            $scope.dish=menuFactory.getdishes().get({id:parseInt($stateParams.id,10)})
+            .$promise.then(function(response){
+                   $scope.dish=response;
+                   $scope.showDish=true;
+               },function(response){
+                   $scope.message='Error'+response.status+response.statusText;
+               });
+            $scope.passVal=function(){
+                console.log($scope.test.val);
+                $scope.criteria=$scope.test.val;
+            };
+        }])
+.controller('FormController',['$scope','menuFactory',function($scope,menuFactory){
+    $scope.formval={
+        author:'',
+        rating:'',
+        comment:'',
+        date:''
+    };
+    $scope.invalidcheck=false;
+    $scope.commentSubmit=function(){
+        console.log($scope.formval);
+        if($scope.formval.rating==''){
+            $scope.invalidcheck=true;
+            console.log('incorrect');
+        }else{
+            var currentDate=new Date();
+            var year=currentDate.getFullYear();
+            var month=currentDate.getMonth()+1;
+            var date=currentDate.getDate();
+            var hour=currentDate.getHours();
+            var minute=currentDate.getMinutes();
+            var sec=currentDate.getSeconds();
+            var milsec=currentDate.getMilliseconds();
+            $scope.formval.date=year+'-'+month+'-'+date+'T'+hour+':'+minute+':'+sec+'.'+milsec+'Z';
+            $scope.dish.comments.push($scope.formval);
+            menuFactory.getdishes().update({id:$scope.dish.id},$scope.dish);
+            $scope.invalidcheck=false;
+            $scope.formval={
+        author:'',
+        rating:'',
+        comment:'',
+        date:''
+    };
+            $scope.formVal.$setPristine();
+            console.log($scope.formval);
+        }
+    };
+}])
+.controller('IndexController',['baseURL','$scope','menuFactory',function(baseURL,$scope,menuFactory){
+    $scope.baseURL=baseURL;
+    $scope.showIndex=false;
+    $scope.message='Loading...'
+    $scope.dish_outline=menuFactory.getDishes().get({id:0}).$promise.then(
+        function(response){
+                   $scope.dish_outline=response;
+                   $scope.showIndex=true;
+               },
+        function(response){
+                   $scope.message='Error'+response.status+response.statusText;
+               });
+}])
+.controller('AboutController',['$scope','corFactory',function($scope,corFactory){
+    $scope.leader_outline=corFactory.getLeaderInfo();
+}])
+;
